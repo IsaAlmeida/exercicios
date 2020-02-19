abd5d2ffcc92e8824e895cef498629c242c23695
IsaAlmeida <dorinha.almeida2016@gmail.com>
2020-02-19 17:54:03 -0300

    Links dos exercicios index.php

---
+++ 20200219/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Agendamento</title>
+    </head>
+    <body> 
+        <p>Atividades git:</p>
+       <br/>
+       <a href ='20200219/Barbearia'>20200219 - Barbearia</a>
+    </body>
+</html>
