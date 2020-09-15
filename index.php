cf109fb52b24fffb4e877b2cb9a20c4a69771aaa
IsaAlmeida <dorinha,almeida2016@gmail.com>
2020-09-15 10:28:21 -0300

    Atividade pendente do dia 11/08/2020

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
         <title>Agendamento</title>
     </head>
     <body> 
-        <p>Atividades git:</p>
+        <h3>Atividades git:</h3>
 		<br/>
-		<a href='20200219/Barbearia'>20200219 - Barbearia</a>
+		<a href="20200811/ex.php"> 11/08/2020 - Exercício Editor de Texto com Jquery </a> <br/>
+	    <a href="20200819/index.php"> 19/08/2020 - Engenharia Reversa </a> <br/>
+		<a href="20200826.individual/exe_ajax.php"> 26/08/2020 - Exercício Ajax </a> <br/>
+		<a href="20200826/index.php"> 26/08/2020 - Exercício Ajax</a> <br/>
+		<a href="20200916/form.php"> 16/09/2020 - Atividade API VIACEP </a> <br/>
+		<a href="projeto/grupoB.php"> 16/09/2020 - Projeto Prático do 1º Bimestre </a> <br/>
        
     </body>
 </html>
